708f0a5fc38883366f8c91630007eab0e96a60bb
ZoroX404 <dhondesushant9812@gmail.com>
2025-05-05 01:49:02 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -45,10 +45,11 @@ async def rename(bot, message):
         
     print("Function called")
     file = getattr(message, message.media.value)
-    filename = file.file_name  
+    filename = file.file_name
+    print("file called")
     msg_key = f"{message.from_user.id}_{message.message_id}"
     UPLOAD_CANCEL[msg_key] = False
-
+    print("upload called")
     if file.file_size > 2000 * 1024 * 1024:
          return await message.reply_text("⚠️ Sorry Bro This Bot Doesn't Support Uploading Files Bigger Than 2GB")
 
