bdbf6cf0c0195bb8ead2ba5ecd614bb6e97c4bb7
ZoroX404 <dhondesushant9812@gmail.com>
2025-05-05 01:18:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,6 +36,7 @@ class Config(object):
     # wes response configuration
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
+    UPLOAD_CANCEL = {}
 
 
 class Txt(object):
