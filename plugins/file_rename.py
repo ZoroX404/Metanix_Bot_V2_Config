dff6fc6c580140febf3547fc9ef982d1808ad83b
ZoroX404 <dhondesushant9812@gmail.com>
2025-05-07 19:50:17 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -19,6 +19,7 @@ app = Client("test", api_id=Config.STRING_API_ID, api_hash=Config.STRING_API_HAS
 
 @Client.on_callback_query(filters.regex("cancel"))
 async def cancel_callback(bot, query: CallbackQuery):
+    print("cancel called.................")
     user_id = callback_query.from_user.id
     msg_id = callback_query.message.id
     msg_key = f"{user_id}_{msg_id}"
